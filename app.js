a774e430626661f021a33f55de7aba9f0b533e6f
govind1530 <mm2406402@gmail.com>
2022-04-27 22:36:55 +0530

    add crypto password

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config()
 require('./config/database').connect()
 const User = require("./model/user")
+const bcrypt = require('bcryptjs');
 const app = express();
 app.use(express.json());
 app.get("/", (req, res) => {
@@ -17,6 +18,14 @@ app.post("/register",async (req,res)=>{
     if(existingUser){
         res.status(400).send("User already exists");
     }
+
+    const myEncPassword = await bcrypt.hash(password,10)
+    const user = User.create({
+        firstName,
+        lastName,
+        email:email.toLowerCase(),
+        password:myEncPassword
+    })
 })
 
 module.exports = app;
